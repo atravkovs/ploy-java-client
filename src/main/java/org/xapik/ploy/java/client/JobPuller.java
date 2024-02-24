bc3d176ec51ddb6c2e9052bb5d9dbe712108de28
Artjoms Travkovs <atravkovs@gmail.com>
2024-02-24 15:12:16 +0200

    Add Asynchronous execution

---
+++ src/main/java/org/xapik/ploy/java/client/JobPuller.java
@@ -0,0 +1,107 @@
+package org.xapik.ploy.java.client;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.xapik.ploy.java.client.handlers.JobExecutor;
+import org.xapik.ploy.java.client.handlers.JobExecutorEntry;
+import org.xapik.ploy.java.client.handlers.JobHandler;
+import org.xapik.ploy.jobworker.CompleteWorkItemRequest;
+import org.xapik.ploy.jobworker.JobWorkerServiceGrpc;
+import org.xapik.ploy.jobworker.WorkRequest;
+import org.xapik.ploy.jobworker.WorkResponse;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class JobPuller {
+
+    private final ObjectMapper objectMapper;
+    private final ApplicationContext applicationContext;
+
+    private ManagedChannel channel;
+    private Map<String, JobExecutorEntry> jobExecutors = new HashMap<>();
+
+    @PostConstruct
+    public void postConstruct() {
+        this.channel = ManagedChannelBuilder.forAddress("localhost", 50051)
+                .usePlaintext()
+                .build();
+        initializeJobExecutor();
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        channel.shutdown();
+    }
+
+    @Scheduled(fixedRate = 500)
+    public void pull() {
+        JobWorkerServiceGrpc.JobWorkerServiceBlockingStub stub = JobWorkerServiceGrpc.newBlockingStub(channel);
+
+        WorkResponse workResponse = stub.getWorkItems(
+                WorkRequest.newBuilder().build()
+        );
+
+
+        workResponse.getWorkitemsList()
+                .forEach((workItem) -> {
+                    try {
+                        JobExecutorEntry jobExecutor = jobExecutors.get(workItem.getJobName());
+
+                        if (jobExecutor == null) {
+                            log.error("JobExecutor not found for {}", workItem.getJobName());
+                            return;
+                        }
+
+                        Object input = objectMapper.readValue(workItem.getInputs(), jobExecutor.getInputClass());
+                        jobExecutor.getMethod().invoke(jobExecutor.getHandler(), input);
+
+                        stub.completeWorkItem(
+                                CompleteWorkItemRequest.newBuilder()
+                                        .setJobId(workItem.getJobId())
+                                        .build()
+                        );
+                    } catch (JsonProcessingException | IllegalAccessException | InvocationTargetException e) {
+                        log.error(e.getMessage(), e);
+                        throw new RuntimeException(e);
+                    }
+                });
+    }
+
+    private void initializeJobExecutor() {
+        Map<String, Object> beans = applicationContext.getBeansWithAnnotation(JobHandler.class);
+
+        beans.forEach((key, jobHandler) -> {
+            Class<?> klass = jobHandler.getClass();
+            for (final Method method : klass.getDeclaredMethods()) {
+                if (method.isAnnotationPresent(JobExecutor.class)) {
+                    JobExecutor jobExecutor = method.getAnnotation(JobExecutor.class);
+
+                    JobExecutorEntry jobExecutorEntry = new JobExecutorEntry();
+                    jobExecutorEntry.setMethod(method);
+                    jobExecutorEntry.setHandler(jobHandler);
+                    jobExecutorEntry.setName(jobExecutor.name());
+                    jobExecutorEntry.setInputClass(jobExecutor.inputType());
+
+                    jobExecutors.put(jobExecutorEntry.getName(), jobExecutorEntry);
+                }
+            }
+        });
+    }
+
+}
--- src/main/java/org/xapik/ploy/java/client/PloyJavaClientApplication.java
+++ src/main/java/org/xapik/ploy/java/client/PloyJavaClientApplication.java
@@ -9,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.xapik.ploy.java.client.handlers.JobExecutor;
 import org.xapik.ploy.java.client.handlers.JobExecutorEntry;
 import org.xapik.ploy.java.client.handlers.JobHandler;
@@ -22,63 +23,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
+@EnableScheduling
 @SpringBootApplication
 public class PloyJavaClientApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext ctx = SpringApplication.run(PloyJavaClientApplication.class, args);
-
-        Map<String, Object> beans = ctx.getBeansWithAnnotation(JobHandler.class);
-        beans.keySet().forEach(bean -> log.info("Bean {}", bean));
-
-        Map<String, JobExecutorEntry> jobExecutors = new HashMap<>();
-
-        beans.forEach((key, jobHandler) -> {
-            Class<?> klass = jobHandler.getClass();
-            for (final Method method : klass.getDeclaredMethods()) {
-                if (method.isAnnotationPresent(JobExecutor.class)) {
-                    JobExecutor jobExecutor = method.getAnnotation(JobExecutor.class);
-
-                    JobExecutorEntry jobExecutorEntry = new JobExecutorEntry();
-                    jobExecutorEntry.setMethod(method);
-                    jobExecutorEntry.setHandler(jobHandler);
-                    jobExecutorEntry.setName(jobExecutor.name());
-                    jobExecutorEntry.setInputClass(jobExecutor.inputType());
-
-                    jobExecutors.put(jobExecutorEntry.getName(), jobExecutorEntry);
-                }
-            }
-        });
-
-        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
-                .usePlaintext()
-                .build();
-
-        JobWorkerServiceGrpc.JobWorkerServiceBlockingStub stub = JobWorkerServiceGrpc.newBlockingStub(channel);
-
-        WorkResponse workResponse = stub.getWorkItems(
-                WorkRequest.newBuilder().build()
-        );
-
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        workResponse.getWorkitemsList()
-                .forEach((workItem) -> {
-                    try {
-                        JobExecutorEntry jobExecutor = jobExecutors.get(workItem.getJobName());
-
-                        if (jobExecutor == null) {
-                            log.error("JobExecutor not found for {}", workItem.getJobName());
-                            return;
-                        }
-
-                        Object input = objectMapper.readValue(workItem.getInputs(), jobExecutor.getInputClass());
-                        jobExecutor.getMethod().invoke(jobExecutor.getHandler(), input);
-                    } catch (JsonProcessingException | IllegalAccessException | InvocationTargetException e) {
-                        throw new RuntimeException(e);
-                    }
-                });
-
-        channel.shutdown();
+        SpringApplication.run(PloyJavaClientApplication.class, args);
     }
 }
